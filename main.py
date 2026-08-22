239b7adc12f7dcf31dad6ce3807e3944719f6d56
MIZOCR7 <myfreelancinglife777@gmail.com>
2026-08-22 15:32:50 +0300

    I think barrels are working :)

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ row3_top = row3_y - 8 * slope
 row2_top = row2_y - 8 * slope
 row1_top = start_y - 5 * slope
 
+
 barrel_img = pygame.transform.scale(pygame.image.load('assets/images/barrels/barrel.png'), (section_width * 1.5, section_height * 2))  
 
 barrel_spawn_time = 360
@@ -93,32 +94,73 @@ class Barrel(pygame.sprite.Sprite):
     self.rect = self.image.get_rect() 
     self.rect.center = (x, y) 
     self.y_change = 0
-    self.x_change = 1 
+    self.dir = 1
+    self.x_change = 1
     self.pos = 0
     self.count = 0
     self.oil_collision = False 
     self.falling = False
     self.check_lad = False 
+    self.was_falling = False 
     self.bottom = self.rect 
   
   
   def update(self, fire_trig):
-    if self.y_change < 8 and not self.falling:
-      self.y_change += 2 
-    for i in range(len(plats)):
-      if self.bottom.colliderect(plats[i]):
-        self.y_change = 0
-        self.falling = False 
-    if self.rect.colliderect(oil_drum):
-      if not self.oil_collision:
-        self.oil_collision = True 
-        if random.randint(0, 4) == 4:
-          fire_trig = True
-    return fire_trig 
+        if self.y_change < 8 and not self.falling:
+            self.y_change += 5
+        for i in range(len(plats)):
+            if self.bottom.colliderect(plats[i]):
+                self.y_change = 0
+                self.falling = False
+        if self.rect.colliderect(oil_drum):
+            if not self.oil_collision:
+                self.oil_collision = True
+                if random.randint(0, 4) == 4:
+                    fire_trig = True
+        if not self.falling:
+            if row5_top >= self.rect.bottom or row3_top >= self.rect.bottom >= row4_top or row1_top > self.rect.bottom >= row2_top:
+                self.x_change = 3
+            else:
+                self.x_change = -3
+        else:
+            self.x_change = 0
+        
+        
+        self.rect.move_ip(self.x_change, self.y_change)
+        if self.rect.top > screen_height:
+            self.kill()
+        if self.count < 15:
+            self.count += 1
+        else:
+            self.count = 0
+            if self.x_change > 0:
+                if self.pos < 3:
+                    self.pos += 1
+                else:
+                    self.pos = 0
+            else:
+                if self.pos > 0:
+                    self.pos -= 1
+                else:
+                    self.pos = 3
+        self.bottom = pygame.rect.Rect((self.rect[0], self.rect.bottom), (self.rect[2], 3))
+        return fire_trig
+
     
     
   def check_fall(self):
-    pass
+    already_collided = False 
+    below = pygame.rect.Rect((self.rect[0], self.rect[1] + section_height), (self.rect[2], section_height)) 
+    for lad in lads:
+      if below.colliderect(lad) and not self.falling and not self.check_lad:
+        self.check_lad = True
+        already_collided = True
+        if random.randint(0, 60) == 60:
+          self.falling = True 
+          self.y_change = 4 
+    
+    if not already_collided:
+      self.check_lad = False 
   
   def draw(self):
     screen.blit(pygame.transform.rotate(barrel_img, 90*self.pos), self.rect.topleft)
@@ -133,7 +175,7 @@ class Bridge:
 
     def draw(self):
         line_width = 7
-        platform_color = (255, 51, 129)
+        platform_color = (225, 51, 129)
         for i in range(self.length):
             bottom = self.y + section_height
             left = self.x + (section_width * i)
@@ -169,7 +211,7 @@ class Ladder:
       pygame.draw.line(screen, lad_color, (left, top), (left, bottom), line_width)
       pygame.draw.line(screen, lad_color, (right, top), (right, bottom), line_width)
       pygame.draw.line(screen, lad_color, (left, center), (right, center), line_width) 
-    body = pygame.rect.Rect((self.x, self.y - section_height), (section_width, (lad_height * self.length + section_height)))
+    body = pygame.rect.Rect((self.x, self.y - section_height), (section_width, (lad_height * self.length * section_height + section_height))) 
     return body 
 
 
@@ -201,30 +243,31 @@ barrels = pygame.sprite.Group()
 oil_drum = pygame.rect.Rect((1,1), (1,1)) 
 
 def main():
-    global barrel_count, barrel_time
-    run = True
-    while run:
+    global barrel_count, barrel_time, plats, lads, fireball_trigger
+    while True:
         screen.fill('black')
         timer.tick(fps)
+        
+        plats, lads = draw_screen() 
         
         if barrel_count < barrel_spawn_time:
           barrel_count += 1
         else:
           barrel_count = random.randint(0, 120) 
-          barrel_time = barrel_count - barrel_spawn_time 
-          barrel = Barrel(270, 270) 
+          barrel_time = barrel_spawn_time - barrel_count
+          barrel = Barrel(int(5 * section_width), int(row6_top - section_height)) 
           barrels.add(barrel) 
           
+        
         for barrel in barrels:
           barrel.draw() 
           barrel.check_fall() 
-        barrel.update() 
-          
-        plats, lads = draw_screen()
+          fireball_trigger = barrel.update(fireball_trigger)  
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                pygame.quit() 
+                sys.exit()
 
         pygame.display.flip()
 
